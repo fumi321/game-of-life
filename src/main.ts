9654bb2fcf9a522f45e7ba1089736be8f9128fd0
fumio <p30f22g50@gmail.com>
2024-03-18 20:44:08 +0900

    Add canvas

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d")!;
