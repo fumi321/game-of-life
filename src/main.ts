a4a4b82ee50af1b3f6751897b145df284e6d8c7c
fumio <p30f22g50@gmail.com>
2024-03-19 15:01:55 +0900

    Add loop process

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import { render, map, randomizeMap } from "./life.ts";
+import { render, Status, randomizeMap, computeNextGeneration } from "./life.ts";
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d")!;
+
+const size = 64;
+let map: Status[][] = new Array(size)
+  .fill(Status.Alive)
+  .map(() => new Array(size).fill(Status.Dead));
+
 randomizeMap(map);
 
-render(context);
+render(context, map);
+
+setInterval(() => {
+  map = computeNextGeneration(map);
+  render(context, map);
+}, 1000);
 
 console.table(map);
