78ced8c8e6a5687f544fb8a57ed0af90f7dc490c
fumio <p30f22g50@gmail.com>
2024-03-19 12:22:24 +0900

    Add randomizeMap

--- src/life.ts
+++ src/life.ts
@@ -9,8 +9,6 @@ export let map: Status[][] = new Array(size)
   .fill(Status.Alive)
   .map(() => new Array(size).fill(Status.Dead));
 
-map[32][32] = Status.Alive;
-
 export function render(context: CanvasRenderingContext2D) {
   const size = window.innerHeight / map.length;
   for (let i = 0; i < map.length; i++) {
@@ -19,6 +17,14 @@ export function render(context: CanvasRenderingContext2D) {
         context.fillStyle = "green";
         context.fillRect(i * size, j * size, size, size);
       }
+    }
+  }
+}
+
+export function randomizeMap(map: Status[][]) {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      map[i][j] = Math.round(Math.random());
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { render, map } from "./life.ts";
+import { render, map, randomizeMap } from "./life.ts";
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d")!;
+randomizeMap(map);
 
 render(context);
 
