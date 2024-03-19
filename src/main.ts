22e6fcae08418206fcd759b6a2d131e553a9c70c
fumio <p30f22g50@gmail.com>
2024-03-19 16:22:31 +0900

    Use randomizeMap

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,7 @@ const context = canvas.getContext("2d")!;
 const size = 64;
 let map: Status[][] = new Array(size).fill([]).map(() => new Array(size).fill(Status.Dead));
 
-map[30][30] = Status.Alive;
-map[30][31] = Status.Alive;
-map[30][32] = Status.Alive;
-map[31][32] = Status.Alive;
-map[32][31] = Status.Alive;
+randomizeMap(map);
 
 setInterval(() => {
   render(context, map);
